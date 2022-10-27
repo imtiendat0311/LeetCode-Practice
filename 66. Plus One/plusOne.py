40317f138612bc027e3116d97dbd9f0f84f52bf0
superboo0311 <60676245+superboo0311@users.noreply.github.com>
2022-10-27 13:10:36 -0400

    update 66 readme  comment

--- 66. Plus One/plusOne.py
+++ 66. Plus One/plusOne.py
@@ -1,3 +1,5 @@
+# faster than 82.44% other online python3 submission
+# use less mem than 96.55% other online python3 submission
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         a = ""
