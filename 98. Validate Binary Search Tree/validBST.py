dc235306ff43d948ad5682a0fde6be2b89436afa
superboo0311 <60676245+superboo0311@users.noreply.github.com>
2022-10-27 15:03:08 -0400

    add 83 and 98

---
+++ 98. Validate Binary Search Tree/validBST.py
@@ -0,0 +1,16 @@
+# Faster than 93.09% online python3 submission
+# Less mem than 13.90% online python3 submission
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        a,_,_= self.something(root)
+        return a 
+    def something(self,root: Optional[TreeNode]):
+        if root is None:
+            return True,float("inf"),-float("inf")
+        if root.left is None and root.right is None:
+            return True,root.val,root.val
+        isleftBST,leftSubMin,leftSubMax = self.something(root.left)
+        isrightBST,rightSubMin,rightSubMax = self.something(root.right)
+        if isleftBST and isrightBST and leftSubMax<root.val<rightSubMin:
+            return True,min(leftSubMin,root.val),max(rightSubMax,root.val)
+        return False,None,None  
