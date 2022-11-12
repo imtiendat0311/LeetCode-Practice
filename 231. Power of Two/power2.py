d330caddc380cb08852ccc216a6e05ddcae9eabf
superboo0311 <60676245+superboo0311@users.noreply.github.com>
2022-11-12 12:03:37 -0500

    adding comment

--- 231. Power of Two/power2.py
+++ 231. Power of Two/power2.py
@@ -1,3 +1,7 @@
+#  faster than 96.02% of Python3 online submissions
+# less than 53.65% of Python3 online submissions for Power of Two.
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
         return n>0 and (n&n-1)==0
