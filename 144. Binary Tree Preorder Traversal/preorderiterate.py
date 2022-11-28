99a21199da9cf12598f8cfd3ba0920283693fffa
superboo0311 <60676245+superboo0311@users.noreply.github.com>
2022-11-27 20:29:09 -0500

    add iterate

--- 144. Binary Tree Preorder Traversal/preorderiterate.py
+++ 144. Binary Tree Preorder Traversal/preorderiterate.py
@@ -1,3 +1,6 @@
+# Runtime: 33 ms, faster than 91.56% of Python3 online submissions for Binary Tree Preorder Traversal.
+# Memory Usage: 13.9 MB, less than 60.31% of Python3 online submissions for Binary Tree Preorder Traversal.
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,7 +8,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if root is None:
             return []
         a,b = [],[(root,False)]
@@ -16,6 +19,6 @@ class Solution:
             else:
                 if c is not None:
                     b.append((c.right,False))
-                    b.append((c,True))
                     b.append((c.left,False))
+                    b.append((c,True))
         return a
--- 145. Binary Tree Postorder Traversal/postorderiterate.py
+++ 145. Binary Tree Postorder Traversal/postorderiterate.py
@@ -1,3 +1,6 @@
+# Runtime: 33 ms, faster than 91.92% of Python3 online submissions for Binary Tree Postorder Traversal.
+# Memory Usage: 13.8 MB, less than 60.40% of Python3 online submissions for Binary Tree Postorder Traversal.
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,7 +8,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if root is None:
             return []
         a,b = [],[(root,False)]
@@ -15,7 +18,7 @@ class Solution:
                 a.append(c.val)
             else:
                 if c is not None:
-                    b.append((c.right,False))
                     b.append((c,True))
+                    b.append((c.right,False))
                     b.append((c.left,False))
         return a
